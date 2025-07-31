dc4a541aa8262bb914423d7c8e5c5d6b167fbd5c
jeremiekoue8 <jeremiekoue8@gmail.com>
2025-07-31 09:53:49 +0000

    set time

--- src/main/java/com/Megatram/Megatram/MegatramApplication.java
+++ src/main/java/com/Megatram/Megatram/MegatramApplication.java
@@ -3,11 +3,16 @@ package com.Megatram.Megatram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MegatramApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MegatramApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Forcer la timezone JVM à UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        SpringApplication.run(MegatramApplication.class, args);
+    }
 
 }
